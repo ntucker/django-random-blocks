e9efdb804a482795622ee68f2aa49f6a1886f7be
Nathaniel Tucker <me@ntucker.me>
2013-05-21 04:29:44 +0000

    Python 3 compatibility

--- randomblocks/models.py
+++ randomblocks/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 import jsonfield
 
 
+@python_2_unicode_compatible
 class Block(models.Model):
     context = jsonfield.JSONField()
     weight = models.IntegerField(default=1)
@@ -12,9 +14,10 @@ class Block(models.Model):
         return self.template.markup.format(**self.context)
 
 
+@python_2_unicode_compatible
 class BlockTemplate(models.Model):
     name = models.CharField(max_length=70)
     markup = models.TextField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
