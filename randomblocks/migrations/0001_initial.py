3f64623aebc49e667af7207a8ccb3f87a9d78802
Nathaniel Tucker <me@ntucker.me>
2015-02-02 00:27:50 +0000

    Use Django 1.7 migrations instead of south

--- randomblocks/migrations/0001_initial.py
+++ randomblocks/migrations/0001_initial.py
@@ -1,53 +1,42 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import jsonfield.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Block'
-        db.create_table(u'randomblocks_block', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('context', self.gf('jsonfield.fields.JSONField')(default={})),
-            ('weight', self.gf('django.db.models.fields.IntegerField')(default=1)),
-            ('template', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['randomblocks.BlockTemplate'])),
-        ))
-        db.send_create_signal(u'randomblocks', ['Block'])
+    dependencies = [
+    ]
 
-        # Adding model 'BlockTemplate'
-        db.create_table(u'randomblocks_blocktemplate', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=70)),
-            ('markup', self.gf('django.db.models.fields.TextField')()),
-        ))
-        db.send_create_signal(u'randomblocks', ['BlockTemplate'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Block'
-        db.delete_table(u'randomblocks_block')
-
-        # Deleting model 'BlockTemplate'
-        db.delete_table(u'randomblocks_blocktemplate')
-
-
-    models = {
-        u'randomblocks.block': {
-            'Meta': {'object_name': 'Block'},
-            'context': ('jsonfield.fields.JSONField', [], {'default': '{}'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'template': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['randomblocks.BlockTemplate']"}),
-            'weight': ('django.db.models.fields.IntegerField', [], {'default': '1'})
-        },
-        u'randomblocks.blocktemplate': {
-            'Meta': {'object_name': 'BlockTemplate'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'markup': ('django.db.models.fields.TextField', [], {}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '70'})
-        }
-    }
-
-    complete_apps = ['randomblocks']
+    operations = [
+        migrations.CreateModel(
+            name='Block',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('context', jsonfield.fields.JSONField(default=dict)),
+                ('weight', models.IntegerField(default=1)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='BlockTemplate',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=70)),
+                ('markup', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='block',
+            name='template',
+            field=models.ForeignKey(to='randomblocks.BlockTemplate'),
+            preserve_default=True,
+        ),
+    ]
